900e50377e1d99e57d3810bd1c04314e97ab692a
Ramiz Ahmed <ramiz.ahmed@letspingup.com>
2024-08-13 15:48:49 +0500

    Add changes

--- src/pages/teacher/StudentPages/index.tsx
+++ src/pages/teacher/StudentPages/index.tsx
@@ -9,9 +9,10 @@ import PageContainer from '../../../components/PageContainer';
 import useFetchStudent from './use-fetch-student';
 import { Button } from '../../../components/ui/button';
 import { Input } from '../../../components/ui/input';
-import { Component1Icon, UploadIcon } from '@radix-ui/react-icons';
+import { Component1Icon, Cross2Icon, UploadIcon } from '@radix-ui/react-icons';
 import { httpClient } from '../../../lib/httpClient';
 import useCreateBoard from '../../../api/use-create-board';
+import { cn } from '../../../lib/utils';
 
 const board: any = [
   {},
@@ -100,24 +101,40 @@ export default function StudentPages({ userId }: { userId?: string }) {
   return (
     <PageContainer className='mt-4'>
       <div className='flex items-center justify-between'>
-        <Button variant='secondary'>User - {student?.username}</Button>
-        {!userId &&
-          (isHome ? (
-            <AssignToStudents pages={pages} />
-          ) : (
-            <Button
-              onClick={() => {
-                mutate({ pages, studentIds: [student?._id as string] });
-              }}
-            >
-              {isPending && <Component1Icon className='mr-2 animate-spin' />}{' '}
-              Save
-            </Button>
-          ))}
+        {isHome ? (
+          <div />
+        ) : (
+          <Button variant='secondary'>User - {student?.username}</Button>
+        )}
+        <div>
+          {!userId &&
+            (isHome ? (
+              <AssignToStudents pages={pages} />
+            ) : (
+              <Button
+                onClick={() => {
+                  mutate({ pages, studentIds: [student?._id as string] });
+                }}
+              >
+                {isPending && <Component1Icon className='mr-2 animate-spin' />}{' '}
+                Save
+              </Button>
+            ))}
+          <Button
+            className='ml-2'
+            onClick={() => {
+              const newPages = [...pages];
+              newPages[selected] = [...board];
+              setPages(newPages);
+            }}
+          >
+            Clear
+          </Button>
+        </div>
       </div>
 
       <section className='mt-4'>
-        <div className='space-x-3 mb-4'>
+        <div className='space-x-2 md:space-x-3 mb-4'>
           {pages?.map((_: any, idx: number) => (
             <Button
               key={idx}
@@ -125,8 +142,39 @@ export default function StudentPages({ userId }: { userId?: string }) {
                 setSelected(idx);
               }}
               variant={idx === selected ? 'default' : 'secondary'}
+              className='py-1 px-2 md:py-2 md:px-4 relative min-w-[80px] text-sm md:text-base md:min-w-[100px]'
             >
-              Page {idx + 1}
+              Page
+              <Button
+                onClick={() => {
+                  if (pages.length > 1) {
+                    const newPages = [...pages];
+                    newPages.splice(idx, 1);
+                    setPages(newPages);
+                    console.log('selected', idx, pages.length);
+                    if (idx + 1 === pages.length) {
+                      setTimeout(() => {
+                        setSelected(idx - 1);
+                      }, 0);
+                      return;
+                    }
+                    if (pages.length === 2) {
+                      console.log('true');
+                      setTimeout(() => {
+                        setSelected(0);
+                      }, 0);
+                    }
+                  }
+                }}
+                className={cn(
+                  'hover:bg-black p-1 absolute top-0 right-0 rounded-md w-4 h-4 md:w-5 md:h-5',
+                  {
+                    'bg-white text-black hover:bg-white': idx === selected,
+                  }
+                )}
+              >
+                <Cross2Icon className='w-3 h-3' />
+              </Button>
             </Button>
           ))}
           {!userId && (
@@ -142,9 +190,9 @@ export default function StudentPages({ userId }: { userId?: string }) {
             </Button>
           )}
         </div>
-        <div className='grid grid-cols-5 bg-slate-100 shadow-md rounded-md gap-2'>
+        <div className='grid grid-cols-3 md:grid-cols-5 border-black border bg-slate-100  rounded-md'>
           {pages[selected]?.map((row: any, idx: number) => (
-            <div className='h-[210px] flex flex-col' key={idx}>
+            <div className='grid-item h-[210px] flex flex-col' key={idx}>
               <div className='flex justify-center items-center min-h-[160px] h-[160px] relative text-center flex-1 mb-2'>
                 {row.image && (
                   <img
@@ -178,7 +226,7 @@ export default function StudentPages({ userId }: { userId?: string }) {
                 }}
                 type='text'
                 placeholder='Description'
-                className='h-[50px] disabled:opacity-100 disabled:cursor-default'
+                className='border-none outline-none shadow-none input-field h-[50px] disabled:opacity-100 disabled:cursor-default'
               />
             </div>
           ))}
